70623d25ed3169f3a963b2c59fc64ee7843dbb00
Arsan <arsan.irianto@gmail.com>
2024-03-13 16:44:07 +0700

    Initial commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+/**
+ * @OA\Info(
+ *    title="Application API",
+ *    version="1.0.0",
+ * )
+ */
+class Controller extends BaseController
+{
+    use AuthorizesRequests, ValidatesRequests;
+}
